52c187e907d93f4419d9fce4574e3b12f026a1d9
IronGun <strannik.kraya575721@gmail.com>
2022-10-27 10:22:37 +0300

    feat: add output to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <vector>
-
-
-using namespace std;
 
 int main(){
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::cout << "Algorithms and Data Structures" << std::endl;
     return 0;
 }
