e2a935fbb4c3f20efa058d798199b035a6183a17
IronGun <strannik.kraya575721@gmail.com>
2022-10-30 16:13:49 +0300

    feat: add TrieNode struct

--- data_structures/trie.cpp
+++ data_structures/trie.cpp
@@ -1,3 +1,36 @@
+/****************************************************************
+ * @file
+ * @brief Trie Data Structure
+ * @details
+ * A trie is a tree-like data structure that is used to store a dynamic set
+ * or associative array where the keys are usually strings
+ * It is used for efficient retrieval of keys in a dataset of strings
+ *
+ * ### Complexity
+ * Build : O(len)
+ * Insert : O(len)
+ * Search : O(len)
+ * Remove : O(len)
+ * Sort : O(n)
+ * Where len is the length of the string and n is the number of strings
+ * Space Complexity : O(len*a)
+ * Where a is the size of the alphabet
+****************************************************************/
+
+#include <iostream>
+#include <vector>
+
+const int ALPHABET_SIZE = 26;
+const std::string ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+struct TrieNode{
+    std::vector<TrieNode*> children;
+    int count;
+    TrieNode(){
+        children.resize(ALPHABET_SIZE, nullptr);
+        count = 0;
+    }
+};
 
 int main(){
     return 0;
