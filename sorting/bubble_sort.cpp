e752580f753c5eadecb7c8f4d8e9b6779b565f39
IronGun <strannik.kraya575721@gmail.com>
2022-12-19 07:58:45 +0300

    fix: invert using of comparator in bubble sort

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -25,7 +25,7 @@ void bubble_sort(std::vector<T> &arr, bool (*func)(T, T)) {
     int n = arr.size();
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (func(arr[j], arr[j + 1])) {
+            if (!func(arr[j], arr[j + 1])) {
                 std::swap(arr[j], arr[j + 1]);
             }
         }
